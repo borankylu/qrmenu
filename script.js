41c6458f1346995508ce0dd5bba9e193a9d3c1f8
Boran <64576958+borankylu@users.noreply.github.com>
2024-02-01 15:00:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,579 @@
+let foodContainer = document.querySelector('.food-container');
+
+const foodItems = [
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+   
+    {
+        FoodName: "Banana Shakes",
+        foodimg: 'images/s1.jpg',
+        price: "RS 150",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Mango Shakes",
+        foodimg: 'images/s2.jpg',
+        price: "RS 150",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adassna Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Aloo Goshat",
+        foodimg: 'images/l4.jpg',
+        price: "RS 200",
+        type: "Mezeler"
+    }
+    ,{
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Aloo Goshat",
+        foodimg: 'images/l4.jpg',
+        price: "RS 200",
+        type: "Mezeler"
+    }
+    ,{
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Aloo Goshat",
+        foodimg: 'images/l4.jpg',
+        price: "RS 200",
+        type: "Mezeler"
+    }
+    ,{
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Aloo Goshat",
+        foodimg: 'images/l4.jpg',
+        price: "RS 200",
+        type: "Mezeler"
+    }
+    ,{
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Mezeler"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Adana Kebap",
+        foodimg: 'images/b1.jpg',
+        price: "200 TL",
+        type: "Izgaralar"
+    },
+    {
+        FoodName: "Aloo Goshat",
+        foodimg: 'images/l4.jpg',
+        price: "RS 200",
+        type: "Mezeler"
+    }
+    ,{
+        FoodName: "Aloo Goshat",
+        foodimg: 'images/l4.jpg',
+        price: "RS 200",
+        type: "Mezeler"
+    }
+];
+
+function filterFoodItems(category) {
+    foodbox.forEach(box => {
+        box.classList.add('d-none');
+        if (category === 'Izgaralar' && box.classList.contains('Izgaralar')) {
+            box.classList.remove('d-none');
+        } else if (category === 'Mezeler' && box.classList.contains('Mezeler')) {
+            box.classList.remove('d-none');
+        }
+    });
+}
+
+const food = foodItems.map(item => {
+    const listitem = `<div class="col-md-6 foodbox ${item.type}">
+        <div class="content b my-2">
+            <div class="c-image">
+                <img src="${item.foodimg}" alt="">
+            </div>
+            <div class="t-n-p my-2">
+                <div class="header">  
+                    <span class="title">${item.FoodName}</span>
+                    <span class="price">${item.price}</span>
+                </div>
+            </div>
+        </div>   
+    </div>`;
+    foodContainer.innerHTML += listitem;
+});
+
+const foodbox = document.querySelectorAll('.foodbox');
+const menu = document.querySelectorAll('ul');
+
+menu.forEach(m => {
+    m.addEventListener('click', e => {
+        console.log(e.target.innerHTML);
+        const selectedCategory = e.target.innerHTML;
+        filterFoodItems(selectedCategory);
+    });
+});
